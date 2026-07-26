9a070111b8efa6950a39a6cdbf45c4b8edc50cbc
ProStrike2448 <ostapenko.andrey03@gmail.com>
2026-07-26 14:52:15 +0300

    Create & migrate main models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    categories = models.ManyToManyField(Category)
+    tags = models.ManyToManyField(Tag)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_date']
