6b0ac6463440c23c699be31475c0817b85847124
ProStrike2448 <ostapenko.andrey03@gmail.com>
2026-07-26 15:07:31 +0300

    Create basic views & templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from . import models
+
+
+class ArticleListView(generic.ListView):
+    model = models.Article
+    context_object_name = 'articles'
+    template_name = 'blog/article_list.html'
+
+
+class ArticleDetailView(generic.DetailView):
+    model = models.Article
+    context_object_name = 'article'
+    template_name = 'blog/article_detail.html'
